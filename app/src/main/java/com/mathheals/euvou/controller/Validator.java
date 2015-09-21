5fc74672dc4f2d81c41e365675bae7b23130a255
Julliana <julliana@julliana-Vostro-5470.(none)>
2015-09-21 20:59:17 -0300

    Issue #1: Creating password validation

---
+++ app/src/main/java/com/mathheals/euvou/controller/Validator.java
@@ -0,0 +1,46 @@
+package com.mathheals.euvou.controller;
+
+import android.widget.EditText;
+
+/**
+ * Created by julliana on 21/09/15.
+ */
+public class Validator {
+    public static boolean hasPasswordSizeValid(String password) {
+        boolean isValid = true;
+
+        if (!(password.length() >= 6)) {
+            isValid = false;
+        }
+
+        return isValid;
+    }
+
+    public static boolean hasPasswordConfirmationValid(String password, String confirmPassword) {
+        boolean isValid = true;
+
+        if (confirmPassword.compareTo(password) != 0) {
+            isValid = false;
+        }
+
+        return isValid;
+    }
+
+    public static boolean isPasswordValid(String password, EditText editPassword, String confirmPassword, EditText confirmPasswordField) {
+        boolean isValid = true;
+
+        if (!hasPasswordSizeValid(password)) {
+            editPassword.requestFocus();
+            editPassword.setError("Para segurança da sua conta informe seis caracteres para sua senha");
+            isValid = false;
+        }
+
+        else if(!hasPasswordConfirmationValid(password, confirmPassword)){
+            confirmPasswordField.requestFocus();
+            confirmPasswordField.setError("Ops, as senhas não conferem");
+            isValid = false;
+        }
+
+        return isValid;
+    }
+}
