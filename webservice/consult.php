b4fd0dc9d4706701814fe75a5c509e36fa546670
VinyPinheiro <viny-pinheiro@hotmail.com>
2015-09-23 22:49:59 -0300

    Creating page to return the select query

---
+++ webservice/consult.php
@@ -0,0 +1,36 @@
+<?php
+require_once("db.php");
+
+/**
+ * Set these constants to your specific database values
+ */
+define("HOST", "mysql.hostinger.com.br");
+define("USERNAME", "u956965464_euvou");
+define("PASSWORD", "loveteam");
+define("DATABASE_NAME", "u956965464_euvou");
+/** 
+ */
+
+if($_POST){
+
+    $query = $_POST['query'];
+
+    $database = new Database(USERNAME, PASSWORD, HOST, DATABASE_NAME);
+
+    $connection = $database->connect();
+
+    if($connection){
+
+        $queryResult = $database->query($query);
+        $result["result"] = $queryResult;
+        $response = json_encode($result);
+        $database->disconnect();
+    }else{
+        $response = json_encode(array("Erro" => 0));
+    }
+}else{
+    $response = json_encode(array("Erro" => 0));
+}
+echo $response;
+
+?>
