97036c66772742e2f28947ef028d79119d48cca7
VinyPinheiro <viny-pinheiro@hotmail.com>
2015-09-23 22:26:13 -0300

    Issue #6 - Connection with php and android

--- app/src/main/java/dao/UserDAO.java
+++ app/src/main/java/dao/UserDAO.java
@@ -13,7 +13,9 @@ public class UserDAO extends DAO{
 
     public String save(String user){
 
-        String query = "INSERT INTO users(username, password) VALUES('"+user+"', '1234')";
+        String query = "INSERT INTO tb_user(nameUser, login,passwordUser,birthDate, email)\n" +
+                "VALUES\n" +
+                "(\"Vinicius Pinheiro\", \"VinyPinheiro\", \"123\", \"1995-02-14\",\"viny-pinheiro@hotmail.com\")";
 
         String result = this.executeQuery(query);
 
