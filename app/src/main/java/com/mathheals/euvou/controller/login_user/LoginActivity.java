82b5531b7511b16c10613e936c6e6c97b8c9a90a
Igor Ribeiro Barbosa Duarte <igor.ribeiro.duarte@gmail.com>
2015-09-28 22:14:12 -0300

    Issue #2: Creating method to validate login on LoginActivity

--- app/src/main/java/com/mathheals/euvou/controller/login_user/LoginActivity.java
+++ app/src/main/java/com/mathheals/euvou/controller/login_user/LoginActivity.java
@@ -1,13 +1,20 @@
 package com.mathheals.euvou.controller.login_user;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.mathheals.euvou.R;
 
-public class LoginActivity extends AppCompatActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import dao.UserDAO;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,5 +42,42 @@ public class LoginActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void verifyLogin(String typedUsername, String typedPassword) {
+        UserDAO userDAO = new UserDAO(LoginActivity.this);
+
+        JSONObject json = userDAO.searchUserByName(typedUsername);
+
+        SharedPreferences pref;
+        pref = getSharedPreferences("idUser", MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = pref.edit();
+
+        if(json!=null){
+            //String password = json.getJSONObject("0").getString("idUser");
+
+            try {
+                String password = json.getJSONObject("0").getString("passwordUser");
+
+                if(password.equals(typedPassword)){
+                    int idUser=Integer.parseInt(json.getJSONObject("0").getString("idUser"));
+
+                    editor.putInt("idUser", idUser);
+                    editor.commit();
+                }else{
+                    editor.putInt("idUser", -1);
+                    editor.commit();
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
