fe603107b755c4d257399802e8d3ab7d58e098d7
VinyPinheiro <viny-pinheiro@hotmail.com>
2015-09-30 11:11:42 -0300

    issue #8: Fix Place model

--- app/src/main/java/model/Place.java
+++ app/src/main/java/model/Place.java
@@ -1,67 +1,45 @@
 package model;
 
-import android.util.Log;
-
 /**
  * Created by geovanni on 09/09/15.
  */
 public class Place {
     private String name;
-    private String comment;
+    private String[] comment;
     private Integer evaluate;
-    private String longitude;
-    private Integer Number;
-    private Integer complement;
-    private String type;
-    private String patio; /* achei estranho, porém deixarei assim para averiguar com o grupo
-                            posteriormente*/
-    private String zipCode;
+    private Double longitude;
+    private Double latitude;
+    private String operation;
+    private String description;
+    private String address;
 
-
-    public String getZipCode() {
-        return zipCode;
+    public Place(String name, String[] comment, Integer evaluate, Double longitude, Double latitude,
+                 String operation, String description, String address) {
+        this.name = name;
+        this.comment = comment;
+        this.evaluate = evaluate;
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.operation = operation;
+        this.description = description;
+        this.address = address;
     }
 
-    public void setZipCode(String zipCode) {
-        try
-        {
-            if(zipCode.length()>0)
-                this.zipCode = zipCode;
-        }catch(NullPointerException exception)
-        {
-
-        }
-    }
 
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
-        try
-        {
-            if(name.length()>0)
-                this.name = name;
-        }catch(NullPointerException exception)
-        {
-
-        }
-
+        this.name = name;
     }
 
-    public String getComment() {
+    public String[] getComment() {
         return comment;
     }
 
-    public void setComment(String comment) {
-        try
-        {
-            this.comment = comment;
-        }catch(NullPointerException exception)
-        {
-
-        }
-
+    public void setComment(String[] comment) {
+        this.comment = comment;
     }
 
     public Integer getEvaluate() {
@@ -69,98 +47,46 @@ public class Place {
     }
 
     public void setEvaluate(Integer evaluate) {
-        try
-        {
-            if(evaluate < 0 || evaluate >5)
-                System.out.println( "Opa! a avaliação deve ser de 0 a 5");
-            else
-                this.evaluate = evaluate;
-        }catch(NumberFormatException exception)
-        {
-
-        }
-
+        this.evaluate = evaluate;
     }
 
-    public String getLongitude() {
+    public Double getLongitude() {
         return longitude;
     }
 
-    public void setLongitude(String longitude) {
-        try
-        {
-            this.longitude = longitude;
-        }catch(NullPointerException exception)
-        {
-
-        }
-
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
     }
 
-    public Integer getNumber() {
-        return Number;
+    public Double getLatitude() {
+        return latitude;
     }
 
-    public void setNumber(Integer number) {
-        try
-        {
-            Number = number;
-
-        }catch(NullPointerException exception)
-        {
-
-        }catch(NumberFormatException except)
-        {
-
-        }
-
-
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
     }
 
-    public Integer getComplement() {
-        return complement;
+    public String getOperation() {
+        return operation;
     }
 
-    public void setComplement(Integer complement) {
-        try
-        {
-            this.complement = complement;
-
-        }catch(NullPointerException exception)
-        {
-
-        }catch(NumberFormatException except)
-        {
-
-        }
+    public void setOperation(String operation) {
+        this.operation = operation;
     }
 
-    public String getType() {
-        return type;
+    public String getDescription() {
+        return description;
     }
 
-    public void setType(String type) {
-        try {
-            this.type = type;
-
-        }catch(NullPointerException exception)
-        {
-
-        }
+    public void setDescription(String description) {
+        this.description = description;
     }
 
-    public String getPatio() {
-        return patio;
+    public String getAddress() {
+        return address;
     }
 
-    public void setPatio(String patio) {
-       try {
-
-           this.patio = patio;
-       }catch(NullPointerException exception)
-       {
-
-       }
+    public void setAddress(String address) {
+        this.address = address;
     }
-
 }
