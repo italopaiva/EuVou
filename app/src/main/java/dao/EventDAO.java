e18ea4eb6f7b5affa438cec404aa201130a2f042
Geovanni Oliveira <geovannirock@gmail.com>
2015-10-10 16:48:03 -0300

    Issue #05 -the class EventDAO with the method saveEvent have been created

---
+++ app/src/main/java/dao/EventDAO.java
@@ -0,0 +1,15 @@
+package dao;
+
+import model.Event;
+
+/**
+ * Created by geovanni on 10/10/15.
+ */
+public class EventDAO extends DAO {
+
+    public void saveEvent(Event event){
+        this.executeQuery("INSERT INTO tb_event(nameEvent,description,latitude,longitude)VALUES"+"(\""+event.getNameEvent()+" \", \""+
+        event.getDescription()+" \", \""+event.getLatitude()+" \", \""+event.getLongitude()+" \")");
+
+    }
+}
