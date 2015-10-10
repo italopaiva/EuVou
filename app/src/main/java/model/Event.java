252fc81e863d9535bdff690ad3a40fdd07f4241b
Geovanni Oliveira <geovannirock@gmail.com>
2015-10-09 23:44:21 -0300

    Issue05 - The method to validate the event description has been done

--- app/src/main/java/model/Event.java
+++ app/src/main/java/model/Event.java
@@ -1,12 +1,15 @@
 package model;
 
 import java.text.DecimalFormat;
-
+//confirmar restrições de tamanho
 import exception.EventException;
 
 public class Event {
     public static final String EVENT_NAME_CANT_BE_EMPTY_NAME = "Hey, acho que você está esquecendo de nos informar o nome do evento.";
-    public static final String NAME_CANT_BE_GREATER_THAN_50 = "Hey, acho que você ultrapassou o número de caracteres permitido para o nome do evento, tente novamente.";
+    public static final String NAME_CANT_BE_GREATER_THAN_50 = "Hey, você ultrapassou o número de caracteres permitido para o nome do evento, tente novamente.";
+    public static  final String DESCRIPTION_CANT_BE_EMPTY = "Hey, acho que você esqueu de informar a descrição do evento.";
+    public static final String DESCRIPTION_CANT_BE_GREATER_THAN = "Hey, o máximo de caractéres para descrever um evento é de 500 caracteres";
+
 
     private int idEvent;
     private String nameEvent;
@@ -15,7 +18,7 @@ public class Event {
     private Float longitude;
 
     private static final int MAX_LENGTH_NAME = 50;
-
+    private static final int MAX_LENGTH_DESCRIPTION = 500;
     public Event(String nameEvent,String description,Float latitude, Float longitude) throws EventException {
         setNameEvent(nameEvent);
         setDescription(description);
@@ -54,8 +57,22 @@ public class Event {
         return description;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public void setDescription(String description) throws EventException{
+        if(!description.isEmpty() && description !=null)
+        {
+            if(description.length() < MAX_LENGTH_DESCRIPTION)
+            {
+                this.description = description;
+            }else
+            {
+                throw new EventException(DESCRIPTION_CANT_BE_GREATER_THAN);
+            }
+
+        }else
+        {
+            throw new EventException(DESCRIPTION_CANT_BE_EMPTY);
+        }
+
     }
 
     public Float getLatitude() {
