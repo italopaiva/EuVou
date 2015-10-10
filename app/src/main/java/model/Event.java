b7c0772f8467793c72d92cd2452495d0dc192418
Geovanni Oliveira <geovannirock@gmail.com>
2015-10-09 22:54:36 -0300

    Issue #05 - The method to validate the event name has been created

--- app/src/main/java/model/Event.java
+++ app/src/main/java/model/Event.java
@@ -2,8 +2,11 @@ package model;
 
 import java.text.DecimalFormat;
 
-public class Event {
+import exception.EventException;
 
+public class Event {
+    public static final String EVENT_NAME_CANT_BE_EMPTY_NAME = "Hey, acho que você está esquecendo de nos informar o nome do evento.";
+    public static final String NAME_CANT_BE_GREATER_THAN_50 = "Hey, acho que você ultrapassou o número de caracteres permitido para o nome do evento, tente novamente.";
 
     private int idEvent;
     private String nameEvent;
@@ -11,8 +14,9 @@ public class Event {
     private Float latitude;
     private Float longitude;
 
-    public Event(String nameEvent,String description,Float latitude, Float longitude)
-    {
+    private static final int MAX_LENGTH_NAME = 50;
+
+    public Event(String nameEvent,String description,Float latitude, Float longitude) throws EventException {
         setNameEvent(nameEvent);
         setDescription(description);
         setLatitude(latitude);
@@ -27,12 +31,23 @@ public class Event {
         this.longitude = longitude;
     }
 
-    public String getNameEvent() {
+    public String getNameEvent(){
         return nameEvent;
     }
 
-    public void setNameEvent(String nameEvent) {
-        this.nameEvent = nameEvent;
+    public void setNameEvent(String nameEvent) throws EventException {
+        if(!nameEvent.isEmpty() && nameEvent!= null) {
+
+            if(nameEvent.length() < MAX_LENGTH_NAME)
+            {
+                this.nameEvent = nameEvent;
+            }else
+            {
+                throw new EventException(NAME_CANT_BE_GREATER_THAN_50);
+            }
+        }else{
+            throw new EventException(EVENT_NAME_CANT_BE_EMPTY_NAME);
+        }
     }
 
     public String getDescription() {
