f3d97c36b19bd18b2ab5bfc75584f1959bcc6b71
Izabela Cardoso <cizabelacristina@gmail.com>
2015-10-15 17:57:41 -0300

    Issue #5: Creating method to add listeners in CheckBoxs

--- app/src/main/java/com/mathheals/euvou/controller/event_registration/RegisterEventFragment.java
+++ app/src/main/java/com/mathheals/euvou/controller/event_registration/RegisterEventFragment.java
@@ -4,6 +4,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import com.mathheals.euvou.R;
 import com.mathheals.euvou.controller.utility.Mask;
@@ -25,15 +26,62 @@ public class RegisterEventFragment extends android.support.v4.app.Fragment imple
         Button registerEvent = (Button) view.findViewById(R.id.saveEvent);
         registerEvent.setOnClickListener(this);
 
+        //Adding listener to eventLocal EditText
+        EditText eventLocal = (EditText) view.findViewById(R.id.eventLocal);
+        eventLocal.setOnClickListener(this);
+
         //Adding mask to eventDate Field
         EditText eventDate = (EditText) view.findViewById(R.id.eventDate);
         eventDate.addTextChangedListener(Mask.insert("##/##/####", eventDate));
+
+        //Adding listener to CheckBoxs to verify if each CheckBox is checked or not
+        addCheckBoxListeners(view);
 
         return view;
     }
 
     @Override
     public void onClick(View v) {
-        //Put here methods actuated by click on saveEvent Button
+        if(v.getId() == R.id.saveEvent){
+            //Put here methods actuated by click on saveEvent Button
+        }
+
+        else if(v.getId() == R.id.eventLocal){
+            //Put here methods actuated by click on eventLocal Field (Map to set local)
+        }
+
     }
+
+    private void addCheckBoxListeners(View v){
+
+        CheckBox showCategory = (CheckBox) v.findViewById(R.id.optionShow);
+        showCategory.setOnClickListener(this);
+
+        CheckBox expositionCategory = (CheckBox) v.findViewById(R.id.optionExposition);
+        showCategory.setOnClickListener(this);
+
+        CheckBox museumCategory = (CheckBox) v.findViewById(R.id.optionMuseum);
+        showCategory.setOnClickListener(this);
+
+        CheckBox cinemaCategory = (CheckBox) v.findViewById(R.id.optionCinema);
+        showCategory.setOnClickListener(this);
+
+        CheckBox theaterCategory = (CheckBox) v.findViewById(R.id.optionTheater);
+        showCategory.setOnClickListener(this);
+
+        CheckBox partyCategory = (CheckBox) v.findViewById(R.id.optionParty);
+        showCategory.setOnClickListener(this);
+
+        CheckBox educationCategory = (CheckBox) v.findViewById(R.id.optionEducation);
+        showCategory.setOnClickListener(this);
+
+        CheckBox sportsCategory = (CheckBox) v.findViewById(R.id.optionSports);
+        showCategory.setOnClickListener(this);
+
+        CheckBox othersCategory = (CheckBox) v.findViewById(R.id.optionOthers);
+        showCategory.setOnClickListener(this);
+
+    }
+
+
 }
