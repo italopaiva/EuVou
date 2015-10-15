052b58cacfa847908cd68184d610ed286925d97c
VinyPinheiro <viny-pinheiro@hotmail.com>
2015-10-14 23:48:40 -0300

    issue #5: Create category entity

--- FISICO.sql
+++ FISICO.sql
@@ -57,6 +57,17 @@ idUser INTEGER NOT NULL,
 dsComment VARCHAR(500) NOT NULL
 )ENGINE = InnoDB;
 
+CREATE TABLE tb_category (
+idCategory INTEGER NOT NULL,
+nameCategory varchar(50) NOT NULL
+)ENGINE = InnoDB;
+
+CREATE TABLE event_category(
+idEvent INTEGER NOT NULL,
+idCategory INTEGER NOT NULL
+)ENGINE = InnoDB;
+
+
 /* PRIMARY KEY */
 
 ALTER TABLE tb_locate ADD CONSTRAINT pk_locate_longitude_latitude 
@@ -71,6 +82,9 @@ ALTER TABLE tb_user ADD CONSTRAINT pk_user_id_user
 ALTER TABLE tb_place ADD CONSTRAINT pk_place_id_place 
 	PRIMARY KEY(idPlace);
 
+ALTER TABLE tb_category ADD CONSTRAINT pk_category_id_category
+	PRIMARY KEY(idCategory);
+
 /* INDEXES*/
 
 CREATE INDEX index_nameEvent ON tb_event(nameEvent(10));
@@ -82,12 +96,15 @@ CREATE INDEX index_namePlace ON tb_place(namePlace(10));
 ALTER TABLE evaluate_user ADD UNIQUE uk_evaluate_use (idUser, idUserEvaluated);
 ALTER TABLE evaluate_place ADD UNIQUE uk_evaluate_place (idUser, idPlace);
 ALTER TABLE participate ADD UNIQUE uk_participate (idUser, idEvent);
+ALTER TABLE event_category ADD UNIQUE uk_category (idCategory, idEvent);
+ALTER TABLE tb_user ADD UNIQUE uk_login (login);
     
 /* AUTO INCREMENTS */
 
 ALTER TABLE tb_user CHANGE COLUMN idUser idUser INTEGER NOT NULL AUTO_INCREMENT;
 ALTER TABLE tb_event CHANGE COLUMN idEvent idEvent INTEGER NOT NULL AUTO_INCREMENT;
 ALTER TABLE tb_place CHANGE COLUMN idPlace idPlace INTEGER NOT NULL AUTO_INCREMENT;
+ALTER TABLE tb_category CHANGE COLUMN idCategory idCategory INTEGER NOT NULL AUTO_INCREMENT;
 
 /* FOREIGN KEY*/
 
@@ -121,12 +138,20 @@ ALTER TABLE tb_comment ADD CONSTRAINT fk_comment_user FOREIGN KEY(idUser)
 ALTER TABLE tb_comment ADD CONSTRAINT fk_comment_event FOREIGN KEY(idEvent)
 	REFERENCES tb_event(idEvent) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
+ALTER TABLE event_category ADD CONSTRAINT fk_event_category_event FOREIGN KEY(idEvent)
+	REFERENCES tb_event(idEvent) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+ALTER TABLE event_category ADD CONSTRAINT fk_event_category_category FOREIGN KEY(idCategory)
+	REFERENCES tb_category(idCategory) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
 /* views */
 CREATE VIEW vw_event AS
-	SELECT tb_event.*, tb_locate.address,AVG(grade) evaluate FROM
+	SELECT tb_event.*, tb_locate.address,AVG(grade) evaluate, nameCategory FROM
 	tb_event INNER JOIN tb_locate ON 
 		(tb_event.latitude = tb_locate.latitude AND 
         tb_event.longitude = tb_locate.longitude)
+	INNER JOIN event_category on tb_event.idEvent = event_category.idEvent
+	INNER JOIN tb_category on tb_category.idCategory = event_category.idCategory
 	LEFT JOIN participate ON participate.idEvent = tb_event.idEvent
     GROUP BY participate.idEvent;
 	
