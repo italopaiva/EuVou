ddda2483fe8788529fecd9a747181fa8ba766ccf
VinyPinheiro <viny-pinheiro@hotmail.com>
2015-10-19 19:57:08 -0200

    issue #05: Fix save event

--- app/src/main/java/dao/EventDAO.java
+++ app/src/main/java/dao/EventDAO.java
@@ -10,12 +10,27 @@ import model.Event;
 public class EventDAO extends DAO {
 
     public void saveEvent(Event event){
-        this.executeQuery("INSERT INTO tb_event(nameEvent,dateTimeEvent,description,longitude,latitude,adress,nameCategory)VALUES"+"(\""+event.getNameEvent()+" \", \""+
-        event.getDateTimeEvent()+" \", \""+event.getDescription()+" \", \""+event.getLongitude()+" \", \""+event.getLatitude()+" \", \""+event.getAdress()+" \", \""+
-                event.getCategory()+" \")");
+        if(executeConsult("Select count(longitude) from tb_locate where longitude = " +
+                event.getLongitude() +" and latitude = " + event.getLatitude()) == null)
+            executeQuery("insert into tb_locate values("+event.getLongitude() +","+event.getLatitude() +
+                    ",'"+event.getAdress()+"')");
+
+        executeQuery("insert into tb_event(nameEvent,dateTimeEvent,description,longitude,latitude) VALUES('" +
+                event.getNameEvent() + "','" + event.getDateTimeEvent() + "','" + event.getDescription() + "'," +
+                "" + event.getLongitude() + "," + event.getLatitude() + ")");
+
+        String query = "";
+        for (String category : event.getCategory()) {
+            query += "INSERT INTO event_category VALUES(" +
+                    "(SELECT idEvent FROM tb_event WHERE nameEvent = '"+event.getNameEvent()+"' AND dateTimeEvent = '"+event.getDateTimeEvent()+"' AND " +
+                    "description = '"+event.getDescription()+"' AND longitude ="+event.getLongitude()+"  AND latitude ="+event.getLatitude()+" )," +
+                    "(SELECT idCategory FROM tb_category WHERE namecategory = '"+category+"'));";
+        }
+
+        executeQuery(query);
 
     }
-    public  void deleteEvent(String eventName)
+    public  void deleteEvent(Event event)
     {
         this.executeQuery("DELETE FROM tb_event WHERE nameEvent =\""+eventName+"\"");
     }
