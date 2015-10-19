00c0c5090f7220cbfc5a8ea742b07bd5d9386277
VinyPinheiro <viny-pinheiro@hotmail.com>
2015-10-19 17:14:07 -0200

    issue #12: First solution for problems with the internet connection

--- app/src/main/java/dao/DAO.java
+++ app/src/main/java/dao/DAO.java
@@ -1,6 +1,7 @@
 package dao;
 
 import android.app.Activity;
+import android.widget.Toast;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
@@ -9,18 +10,29 @@ import org.json.JSONObject;
 import org.json.JSONStringer;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public abstract class DAO {
 
     private final String urlQuery = "http://euvou.esy.es/query.php";
     private final String urlConsult = "http://euvou.esy.es/consult.php";
-    protected Activity activity;
+    private long timeLimit;
+    private final int LIMITCONECTIONTIME = 15000;
 
     protected String executeQuery(String query){
         Consult consult = new Consult(query,urlQuery);
         consult.exec();
-        while(!consult.getIsDoing());
+
+        long currentTime = Calendar.getInstance().getTime().getTime();
+        timeLimit = currentTime + LIMITCONECTIONTIME;
+
+        while(!consult.getIsDoing() && currentTime < timeLimit)
+            currentTime = Calendar.getInstance().getTime().getTime();
+
+        if(currentTime >= timeLimit) {
+            return null;
+        }
         return consult.getResult();
     }
 
@@ -29,7 +41,16 @@ public abstract class DAO {
         String json;
         Consult consult = new Consult(query,urlConsult);
         consult.exec();
-        while(!consult.getIsDoing());
+
+        long currentTime = Calendar.getInstance().getTime().getTime();
+        timeLimit = currentTime + LIMITCONECTIONTIME;
+
+        while(!consult.getIsDoing() && currentTime < timeLimit)
+            currentTime = Calendar.getInstance().getTime().getTime();
+
+        if(currentTime >= timeLimit) {
+            return null;
+        }
 
         json = consult.getResult();
         JSONObject jObject = null;
