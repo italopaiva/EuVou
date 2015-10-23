efb16636d1d330b64514d4433128427f6c5c0a56
Geovanni Oliveira <geovannirock@gmail.com>
2015-10-23 17:51:05 -0200

    The ShowEvent class now has the method to show an event, it's just for a while, the class will be improved.

--- app/src/main/java/com/mathheals/euvou/controller/show_event/ShowEvent.java
+++ app/src/main/java/com/mathheals/euvou/controller/show_event/ShowEvent.java
@@ -7,8 +7,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.mathheals.euvou.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import dao.EventDAO;
+import model.Event;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -65,7 +74,35 @@ public class ShowEvent extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_show_event,container,false);
+        EventDAO eventDAO = new EventDAO(this.getActivity());
+        String eventName =  this.getArguments().getString("eventName");
+        JSONObject eventDATA = eventDAO.searchEventByName(eventName);
+
+        try
+        {
+            String eventNameDB = eventDATA.getJSONObject("0").getString("eventName");
+            String eventAdress = eventDATA.getJSONObject("0").getString("dateTimeEvent");
+            String eventDescription = eventDATA.getJSONObject("0").getString("eventDescription");
+            String eventDateTime = eventDATA.getJSONObject("0").getString("dateTimeEvent");
+            String eventLongitude = eventDATA.getJSONObject("0").getString("longitude");
+            String eventLatitude = eventDATA.getJSONObject("0").getString("latitude");
+
+            TextView nameShow = null, adressShow= null,descriptionShow= null, dataShow = null;
+
+            nameShow.setText(eventNameDB);
+            adressShow.setText(eventAdress);
+            descriptionShow.setText(eventDescription);
+            dataShow.setText(eventDateTime);
+
+        }catch(JSONException ex)
+        {
+
+        }catch(NullPointerException exception)
+        {
+            Toast.makeText(getActivity(),"O nome não foi encontrado",Toast.LENGTH_LONG);
+        }
+
         return inflater.inflate(R.layout.fragment_show_event, container, false);
     }
 
