1dd80edf54c5d66af5ced92c1a5e96e83b5e3d82
Izabela Cardoso <cizabelacristina@gmail.com>
2015-11-03 21:51:02 -0200

    Issue #11: Creating user interactions test for User Registration

--- app/src/androidTest/java/com/mathheals/euvou/UserRegistrationControlTest.java
+++ app/src/androidTest/java/com/mathheals/euvou/UserRegistrationControlTest.java
@@ -8,6 +8,7 @@ import org.junit.Before;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -28,10 +29,123 @@ public class UserRegistrationControlTest extends ActivityInstrumentationTestCase
         getActivity();
     }
 
-    public void testRegisterOptionInActionBarMenu() throws InterruptedException {
+    public void testRegisterOptionInActionBarMenu() {
         openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
         onView(withText("Cadastrar")).perform(click());
         onView(withId(R.id.saveButton)).check(matches(isDisplayed()));
         onView(withId(R.id.saveButton)).check(matches(withText("Cadastrar")));
     }
+    
+    public void testIfNameLabelIsCorrect() {
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        onView(withText("Cadastrar")).perform(click());
+        onView(withId(R.id.userNameLabel)).check(matches(isDisplayed()));
+        onView(withId(R.id.userNameLabel)).check(matches(withText("Nome")));
+    }
+
+    public void testNameField(){
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        onView(withText("Cadastrar")).perform(click());
+        onView(withId(R.id.nameField)).check(matches(isDisplayed()));
+        onView(withId(R.id.nameField)).perform(typeText("Ju Almeida"));
+        onView(withId(R.id.dateField)).perform(click());
+        onView(withId(R.id.nameField)).check(matches(withText("Ju Almeida")));
+    }
+
+    public void testIfDateLabelIsCorrect() {
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        onView(withText("Cadastrar")).perform(click());
+        onView(withId(R.id.birthDateLabel)).check(matches(isDisplayed()));
+        onView(withId(R.id.birthDateLabel)).check(matches(withText("Data de Nascimento")));
+    }
+
+    public void testDateField(){
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        onView(withText("Cadastrar")).perform(click());
+        onView(withId(R.id.dateField)).check(matches(isDisplayed()));
+        onView(withId(R.id.dateField)).perform(typeText("22/12/2050"));
+        onView(withId(R.id.mailField)).perform(click());
+        onView(withId(R.id.dateField)).check(matches(withText("22/12/2050")));
+    }
+
+    public void testIfMailLabelIsCorrect() {
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        onView(withText("Cadastrar")).perform(click());
+        onView(withId(R.id.userMailLabel)).check(matches(isDisplayed()));
+        onView(withId(R.id.userMailLabel)).check(matches(withText("E-mail")));
+    }
+
+    public void testMailField(){
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        onView(withText("Cadastrar")).perform(click());
+        onView(withId(R.id.mailField)).check(matches(isDisplayed()));
+        onView(withId(R.id.mailField)).perform(typeText("oi-xau@voltei.com"));
+        onView(withId(R.id.confirmMailField)).perform(click());
+        onView(withId(R.id.mailField)).check(matches(withText("oi-xau@voltei.com")));
+    }
+
+    public void testIfMailConfirmationLabelIsCorrect() {
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        onView(withText("Cadastrar")).perform(click());
+        onView(withId(R.id.confirmUserMailLabel)).check(matches(isDisplayed()));
+        onView(withId(R.id.confirmUserMailLabel)).check(matches(withText("Confirme o e-mail")));
+    }
+
+    public void testMailConfirmationField(){
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        onView(withText("Cadastrar")).perform(click());
+        onView(withId(R.id.confirmMailField)).check(matches(isDisplayed()));
+        onView(withId(R.id.confirmMailField)).perform(typeText("oi-xau@voltei.com"));
+        onView(withId(R.id.loginField)).perform(click());
+        onView(withId(R.id.confirmMailField)).check(matches(withText("oi-xau@voltei.com")));
+    }
+
+    public void testIfLoginLabelIsCorrect() {
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        onView(withText("Cadastrar")).perform(click());
+        onView(withId(R.id.userLoginLabel)).check(matches(isDisplayed()));
+        onView(withId(R.id.userLoginLabel)).check(matches(withText("Login")));
+    }
+
+    public void testLoginField(){
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        onView(withText("Cadastrar")).perform(click());
+        onView(withId(R.id.loginField)).check(matches(isDisplayed()));
+        onView(withId(R.id.loginField)).perform(typeText("oioioi123"));
+        onView(withId(R.id.passwordField)).perform(click());
+        onView(withId(R.id.loginField)).check(matches(withText("oioioi123")));
+    }
+
+    public void testIfPasswordLabelIsCorrect() {
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        onView(withText("Cadastrar")).perform(click());
+        onView(withId(R.id.userPasswordLabel)).check(matches(isDisplayed()));
+        onView(withId(R.id.userPasswordLabel)).check(matches(withText("Senha")));
+    }
+
+    public void testPasswordField(){
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        onView(withText("Cadastrar")).perform(click());
+        onView(withId(R.id.passwordField)).check(matches(isDisplayed()));
+        onView(withId(R.id.passwordField)).perform(typeText("1234567"));
+        onView(withId(R.id.confirmMailPassword)).perform(click());
+        onView(withId(R.id.passwordField)).check(matches(withText("1234567")));
+    }
+
+    public void testIfConfirmPasswordLabelIsCorrect() {
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        onView(withText("Cadastrar")).perform(click());
+        onView(withId(R.id.userConfirmPasswordLabel)).check(matches(isDisplayed()));
+        onView(withId(R.id.userConfirmPasswordLabel)).check(matches(withText("Confirme a senha")));
+    }
+
+    public void testConfirmPasswordField(){
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        onView(withText("Cadastrar")).perform(click());
+        onView(withId(R.id.confirmMailPassword)).check(matches(isDisplayed()));
+        onView(withId(R.id.confirmMailPassword)).perform(typeText("1234567"));
+        onView(withId(R.id.passwordField)).perform(click());
+        onView(withId(R.id.confirmMailPassword)).check(matches(withText("1234567")));
+    }
+
 }
