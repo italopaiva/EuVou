9d5552602851188081d81fc29d83df5b492a347f
Marlon Mendes <marlonacademy@hotmail.com>
2015-11-04 23:33:20 -0200

    Issue #7: testing if any event was found by user query in event consultation

--- app/src/androidTest/java/com/mathheals/euvou/EventConsultationTest.java
+++ app/src/androidTest/java/com/mathheals/euvou/EventConsultationTest.java
@@ -2,10 +2,12 @@ package com.mathheals.euvou;
 
 import android.test.ActivityInstrumentationTestCase2;
 
+import com.mathheals.euvou.controller.event_consultation.EventConsultation;
 import com.mathheals.euvou.controller.home_page.HomePage;
 
 import org.junit.Before;
 
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 
 import android.support.test.InstrumentationRegistry;
@@ -13,13 +15,19 @@ import android.test.ActivityInstrumentationTestCase2;
 import com.mathheals.euvou.controller.home_page.HomePage;
 import org.junit.Before;
 
+import android.widget.EditText;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.pressKey;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.hasToString;
+import static org.hamcrest.core.StringStartsWith.startsWith;
 
 /**
  * Created by marlonmendes on 04/11/15.
@@ -40,5 +48,15 @@ public class EventConsultationTest extends ActivityInstrumentationTestCase2<Home
         onView(withId(R.id.radio_events)).check(matches(isDisplayed()));
         onView(withId(R.id.radio_people)).check(matches(isDisplayed()));
         onView(withId(R.id.radio_places)).check(matches(isDisplayed()));
+    }
+
+    public void testIfAnyEventWasFound() {
+        onView(withId(R.id.search)).perform(click());
+        onView(isAssignableFrom(EditText.class)).perform(typeText("t"), pressKey(66));
+        onData(hasToString(containsString("t")))
+                .inAdapterView(withId(R.id.events_list)).atPosition(0)
+                .perform(click());
+        onView(withId(R.id.event_name_text)).check(matches(isDisplayed()));
+
     }
 }
