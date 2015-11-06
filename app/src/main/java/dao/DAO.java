faf9b7755a35b103a524fbc09b685652efc4928d
Julliana Almeida <julliana.coutoalmeida@gmail.com>
2015-11-06 04:15:34 -0200

    Issue #6: Adding void constructor to DAO

--- app/src/main/java/dao/DAO.java
+++ app/src/main/java/dao/DAO.java
@@ -3,22 +3,23 @@ package dao;
 import android.app.Activity;
 import android.widget.Toast;
 
-import org.apache.http.NameValuePair;
-import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONObject;
 
 import java.util.Calendar;
 
 public abstract class DAO {
 
-    private final String URLQUERY = "http://euvou.esy.es/query.php";
-    private final String URLCONSULT = "http://euvou.esy.es/consult.php";
+    private final String URLQUERY = "http://euvou.atwebpages.com/query.php";
+    private final String URLCONSULT = "http://euvou.atwebpages.com/consult.php";
     private long timeLimit;
     private final int LIMITCONECTIONTIME = 15000;
     private Activity currentActivity;
+
     public DAO(Activity currentActivity){
         this.currentActivity = currentActivity;
     }
+
+    public DAO(){}
 
     private String query(String query,String urlQuery)
     {
