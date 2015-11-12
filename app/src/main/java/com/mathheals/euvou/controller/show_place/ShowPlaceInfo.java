1836d9de0b21f866d3e49e9ab60d58b352d7d0d5
Geovanni Oliveira <geovannirock@gmail.com>
2015-11-12 02:06:57 -0200

    Issue #15: Now, the places can be avaliated

--- app/src/main/java/com/mathheals/euvou/controller/show_place/ShowPlaceInfo.java
+++ app/src/main/java/com/mathheals/euvou/controller/show_place/ShowPlaceInfo.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Button;
@@ -44,6 +45,10 @@ public class ShowPlaceInfo extends FragmentActivity {
     private Button hideMapButton;
     private SupportMapFragment mMapFragment;
 
+    private RatingBar ratingBar;
+    private TextView txtRatingValue;
+    private Button btnSubmit;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +60,8 @@ public class ShowPlaceInfo extends FragmentActivity {
         setAllTextViews();
         setUpMapIfNeeded();
         mMapFragment.getView().setVisibility(View.INVISIBLE);
+        addListenerOnRatingBar();
+        addListenerOnButton();
     }
 
     @Override
@@ -62,6 +69,40 @@ public class ShowPlaceInfo extends FragmentActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_show_place_info, menu);
         return true;
+    }
+
+    public void addListenerOnRatingBar()
+    {
+        ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                txtRatingValue = (TextView) findViewById(R.id.txtRatingValue);
+                txtRatingValue.setText(String.valueOf(rating));
+            }
+        });
+    }
+    public void addListenerOnButton() {
+
+        ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+        btnSubmit = (Button) findViewById(R.id.btnSubmit);
+
+        //if click on me, then display the current rating value.
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getBaseContext(),
+                        String.valueOf(ratingBar.getRating()),
+                        Toast.LENGTH_SHORT).show();
+
+
+            }
+
+        });
+
+
     }
 
     @Override
