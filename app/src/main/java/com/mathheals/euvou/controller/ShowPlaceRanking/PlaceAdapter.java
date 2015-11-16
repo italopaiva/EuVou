f9fb8cbd8a401b967d42412e27549b64b52933c8
Geovanni Oliveira <geovannirock@gmail.com>
2015-11-16 20:25:10 -0200

    Issue #20: The adater performance has been improved

--- app/src/main/java/com/mathheals/euvou/controller/ShowPlaceRanking/PlaceAdapter.java
+++ app/src/main/java/com/mathheals/euvou/controller/ShowPlaceRanking/PlaceAdapter.java
@@ -21,19 +21,29 @@ public class PlaceAdapter extends ArrayAdapter<Place> {
     public PlaceAdapter(Context context, List<Place> places) {
         super(context, 0,places);
     }
+    private static class ViewHolder {
+        TextView placeName;
+        TextView placeEvaluation;
+    }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
         Place place = getItem(position);
+        ViewHolder viewHolder;
         if (convertView == null) {
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(getContext());
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.fragment_show_place_ranking, parent, false);
+            viewHolder.placeName = (TextView) convertView.findViewById(R.id.placeName);
+            viewHolder.placeEvaluation = (TextView) convertView.findViewById(R.id.placeEvaluation);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
         }
-        TextView placeName = (TextView) convertView.findViewById(R.id.placeName);
-        TextView placeEvaluation = (TextView) convertView.findViewById(R.id.placeEvaluation);
+        viewHolder.placeName.setText(place.getName());
+        viewHolder.placeEvaluation.setText(place.getEvaluate().toString());
 
-        placeName.setText(place.getName());
-        placeEvaluation.setText(place.getEvaluate().toString());
         return convertView;
     }
 }
