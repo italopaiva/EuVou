8d138df4813dd513703c5214c88e05b5b132cf84
Geovanni Oliveira <geovannirock@gmail.com>
2015-11-16 16:38:16 -0200

    A method to get all the places has been created

--- app/src/main/java/dao/PlaceDAO.java
+++ app/src/main/java/dao/PlaceDAO.java
@@ -21,4 +21,8 @@ public class PlaceDAO extends DAO {
     {
         return this.executeConsult("SELECT * FROM vw_place WHERE namePlace LIKE '%" + name + "%'");
     }
+    public JSONObject searchAllPlaces()
+    {
+        return this.executeConsult("SELECT * FROM vw_place");
+    }
 }
