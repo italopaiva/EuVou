bcceb7b16a311b4f9395cd4b82680d26aa013a05
VinyPinheiro <viny-pinheiro@hotmail.com>
2015-11-17 11:21:49 -0200

    issue #20: improving graphical interface

--- app/src/main/java/com/mathheals/euvou/controller/showPlaceRanking/PlaceAdapter.java
+++ app/src/main/java/com/mathheals/euvou/controller/showPlaceRanking/PlaceAdapter.java
@@ -32,7 +32,6 @@ public class PlaceAdapter extends ArrayAdapter<Place> {
         ViewHolder viewHolder;
         if (convertView == null) {
             viewHolder = new ViewHolder();
-            LayoutInflater inflater = LayoutInflater.from(getContext());
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.fragment_show_place_rank, parent, false);
             viewHolder.placeName = (TextView) convertView.findViewById(R.id.placeName);
             viewHolder.placeEvaluation = (TextView) convertView.findViewById(R.id.placeEvaluation);
@@ -41,7 +40,7 @@ public class PlaceAdapter extends ArrayAdapter<Place> {
             viewHolder = (ViewHolder) convertView.getTag();
         }
         viewHolder.placeName.setText(
-                ((place.getName().length() > 30) ? place.getName().substring(0, 29).concat("...") : place.getName()));
+                ((place.getName().length() > 40) ? place.getName().substring(0, 39).concat("...") : place.getName()));
         viewHolder.placeEvaluation.setText(place.getEvaluate().toString());
 
         return convertView;
