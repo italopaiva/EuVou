2a98778837f9b2382f9243434b3f7be89448da65
Geovanni Oliveira <geovannirock@gmail.com>
2015-11-20 01:04:23 -0200

    Issue #20 : The tests to check if the top5 ranking is displayed and if the complete ranking is displayed have been created

---
+++ app/src/androidTest/java/com/mathheals/euvou/ShowPlaceRankingTest.java
@@ -0,0 +1,44 @@
+package com.mathheals.euvou;
+
+import android.support.test.rule.ActivityTestRule;
+import android.test.ActivityInstrumentationTestCase2;
+import com.mathheals.euvou.controller.home_page.HomePage;
+import org.junit.Before;
+import org.junit.Rule;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+/**
+ * Created by geovanni on 18/11/15.
+ */
+public class ShowPlaceRankingTest extends ActivityInstrumentationTestCase2<HomePage> {
+
+    public ShowPlaceRankingTest()  {
+        super(HomePage.class);
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        getActivity();
+    }
+
+    public void testIfTop5IsDisplayed(){
+        onView(withId(R.id.rateEvent)).check(matches(isDisplayed()));
+        onView(withId(R.id.more)).check(matches(isDisplayed()));
+        onView(withId(R.id.labelRankingPlace)).check(matches(isDisplayed()));
+
+    }
+    public void testIfPlaceRakingIsDisplayed(){
+        testIfTop5IsDisplayed();
+        onView(withId(R.id.more)).perform(click());
+        onView(withId(R.id.rateEvent)).check(matches(isDisplayed()));
+
+    }
+
+
+}
