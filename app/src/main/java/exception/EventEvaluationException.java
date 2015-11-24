47e0f0d14687b59025d7b0df25f3c3359e2a2e5f
Izabela Cardoso <cizabelacristina@gmail.com>
2015-11-23 23:30:30 -0200

    Issue #22: Creating Event Evaluation Validation

---
+++ app/src/main/java/exception/EventEvaluationException.java
@@ -0,0 +1,10 @@
+package exception;
+
+import model.EventEvaluation;
+
+/**
+ * Created by izabela on 23/11/15.
+ */
+public class EventEvaluationException extends Exception{
+        public EventEvaluationException(String message) {super(message);}
+}
