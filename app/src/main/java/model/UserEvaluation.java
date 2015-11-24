e3bb55b2c82d92b05d13d6020cbc83f8228c8aa1
Izabela Cardoso <cizabelacristina@gmail.com>
2015-11-23 23:39:13 -0200

    Issue #22: Creating User Evaluation Validation

--- app/src/main/java/model/UserEvaluation.java
+++ app/src/main/java/model/UserEvaluation.java
@@ -1,5 +1,8 @@
 package model;
 
+import exception.EventEvaluationException;
+import exception.UserEvaluationException;
+
 /**
  * Created by igor on 20/11/15.
  */
@@ -7,8 +10,11 @@ public class UserEvaluation {
     private Float rating;
     private Integer userId;
     private Integer userEvaluatedId;
+    public static final String EVALUATION_IS_INVALID = "Hey, a avaliação deve estar entre 0 e 5";
+    public static final String USER_ID_IS_INVALID = "O identificador do usuário é inválido";
+    public static final String USER_EVALUATED_ID_IS_INVALID = "O identificador do usuário avaliado é inválido";
 
-    public UserEvaluation(Float rating, Integer userId, Integer userEvaluatedId){
+    public UserEvaluation(Float rating, Integer userId, Integer userEvaluatedId) throws UserEvaluationException {
         setRating(rating);
         setUserId(userId);
         setUserEvaluatedId(userEvaluatedId);
@@ -18,23 +24,38 @@ public class UserEvaluation {
         return rating;
     }
 
-    public void setRating(Float rating) {
-        this.rating = rating;
+    public void setRating(Float rating) throws UserEvaluationException {
+        if(rating>=0f && rating<=5f) {
+            this.rating = rating;
+        }
+        else{
+            throw new UserEvaluationException(EVALUATION_IS_INVALID);
+        }
     }
 
     public Integer getUserId() {
         return userId;
     }
 
-    public void setUserId(Integer userId) {
-        this.userId = userId;
+    public void setUserId(Integer userId) throws UserEvaluationException {
+        if(userId <= Integer.MAX_VALUE && userId >= 1) {
+            this.userId = userId;
+        }
+        else{
+            throw new UserEvaluationException(USER_ID_IS_INVALID);
+        }
     }
 
     public Integer getUserEvaluatedId() {
         return userEvaluatedId;
     }
 
-    public void setUserEvaluatedId(Integer userEvaluatedId) {
-        this.userEvaluatedId = userEvaluatedId;
+    public void setUserEvaluatedId(Integer userEvaluatedId) throws UserEvaluationException {
+        if(userEvaluatedId <= Integer.MAX_VALUE && userEvaluatedId >= 1) {
+            this.userEvaluatedId = userEvaluatedId;
+        }
+        else {
+            throw new UserEvaluationException(USER_EVALUATED_ID_IS_INVALID);
+        }
     }
 }
