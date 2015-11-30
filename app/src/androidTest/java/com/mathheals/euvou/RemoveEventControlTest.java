8ee31d626f6d3300d345ab3b6f6f4c6ef8b0e3bb
Izabela Cardoso <cizabelacristina@gmail.com>
2015-11-29 23:40:57 -0200

    refactoringApproval: Creating RemoveEventControlTest

---
+++ app/src/androidTest/java/com/mathheals/euvou/RemoveEventControlTest.java
@@ -0,0 +1,75 @@
+package com.mathheals.euvou;
+
+import android.support.test.InstrumentationRegistry;
+import android.support.test.uiautomator.UiDevice;
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.mathheals.euvou.controller.home_page.HomePage;
+import com.mathheals.euvou.controller.utility.LoginUtility;
+
+import org.junit.Before;
+
+import java.text.ParseException;
+import java.util.Vector;
+
+import dao.EventDAO;
+import dao.UserDAO;
+import exception.EventException;
+import model.Event;
+import model.User;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.hasToString;
+
+/**
+ * Created by izabela on 29/11/15.
+ */
+public class RemoveEventControlTest extends ActivityInstrumentationTestCase2<HomePage> {
+
+    private LoginUtility isLoged;
+    private TestUtility setLogin;
+    private UiDevice device;
+    private User user;
+    private EventDAO eventDao = new EventDAO();
+
+    public RemoveEventControlTest() {
+        super(HomePage.class);
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        getActivity();
+        isLoged = new LoginUtility(getActivity());
+        device = UiDevice.getInstance(getInstrumentation());
+    }
+
+    public void testSomething() throws ParseException, EventException {
+
+        if(!isLoged.hasUserLoggedIn()){
+            setLogin.makeUserLogIn();
+        }
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        onView(withText("Meus Eventos")).perform(click());
+
+        onData(hasToString(containsString("Teste")))
+                .inAdapterView(withId(R.id.eventList)).atPosition(0)
+                .perform(click());
+        onView(withId(R.id.editRemoveButton)).perform(click());
+        onView(withId(R.id.removeEvent)).perform(scrollTo());
+        onView(withId(R.id.removeEvent)).perform(click());
+        onView(withText("Deletado com sucesso")).inRoot(withDecorView(not(is(getActivity().getWindow().getDecorView())))).check(matches(isDisplayed()));
+    }
+}
